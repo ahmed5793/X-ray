2929b5819a5940a5995515d6f7611ff128dd7d02
mohammednasser22 <58112817+mohammednasser22@users.noreply.github.com>
2019-12-02 21:09:52 +0200

    Update Program.cs

--- Laboratory/Program.cs
+++ Laboratory/Program.cs
@@ -21,22 +21,7 @@ namespace Laboratory
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-
-
-
-
-      
-
-  
-
-
-
-
-
-
-           
-
-
+            Application.Run(new Frm_Main());
         }
     }
 }
