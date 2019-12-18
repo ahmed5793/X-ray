e611b1f69d27f772a7dea391600509fcaefd7667
mohammednasser22 <58112817+mohammednasser22@users.noreply.github.com>
2019-12-18 21:31:28 +0200

    ssssssssssss

--- Laboratory/BL/CategoryXRaya.cs
+++ Laboratory/BL/CategoryXRaya.cs
@@ -63,6 +63,19 @@ namespace Laboratory.BL
             da.close();
             return dt;
         }
+        internal DataTable Validate_CategoryXray(int id)
+        {
+            DataTable dt = new DataTable();
+
+            DataAccessLayer da = new DataAccessLayer();
+            da.open();
+            SqlParameter[] param = new SqlParameter[1];
+            param[0] = new SqlParameter("@id", SqlDbType.Int);
+            param[0].Value = id;
+            dt = da.selected("Validate_CategoryXray", param);
+            da.close();
+            return dt;
+        }
     }
 }
 
