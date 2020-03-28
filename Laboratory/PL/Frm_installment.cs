1d4bfb3373510737246b24d37a63eb4d1343cee1
mohammednasser22 <58112817+mohammednasser22@users.noreply.github.com>
2020-03-28 22:01:37 +0200

    q

--- Laboratory/PL/Frm_installment.cs
+++ Laboratory/PL/Frm_installment.cs
@@ -28,7 +28,7 @@ namespace Laboratory.PL
             cmb_instalmenttype.DisplayMember = "Name";
             cmb_instalmenttype.ValueMember = "ID_installmentType";
             Btn_Update.Enabled = false;
-
+            textBox1.Hide();
         }
 
         private void Frm_installment_Load(object sender, EventArgs e)
@@ -61,7 +61,6 @@ namespace Laboratory.PL
         {
             if (dataGridView1.Rows.Count>0)
             {
-       
                 Btn_Update.Enabled = true;
                 cmb_stock.Enabled = false;
                 Btn_Add.Enabled = false;
@@ -70,22 +69,16 @@ namespace Laboratory.PL
                 txt_money.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
                 dateTimePicker1.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
                 cmb_instalmenttype.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
-              
+                textBox1.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();              
             }
         }
 
         private void btn_new_Click(object sender, EventArgs e)
-        {
-            
+        {            
         }
-
         private void btn_update_Click(object sender, EventArgs e)
-        {
-           
-             
-           
+        {                                   
         }
-
         private void txt_money_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8 && e.KeyChar != Convert.ToChar(System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator))
@@ -104,6 +97,7 @@ namespace Laboratory.PL
             Btn_Update.Enabled = false;
             cmb_stock.Enabled = true;
             Btn_Add.Enabled = true;
+            textBox1.Clear();
         }
         private void simpleButton1_Click(object sender, EventArgs e)
         {
@@ -112,7 +106,6 @@ namespace Laboratory.PL
 
         private void Btn_Add_Click(object sender, EventArgs e)
         {
-
             try
             {
                 if (txt_money.Text == "" || txt_money.Text == "0")
@@ -132,13 +125,11 @@ namespace Laboratory.PL
                     cmb_instalmenttype.Focus();
                     return;
                 }
+
                 dt.Clear();
-
                 dt = s.Select_moneyStock(Convert.ToInt32(cmb_stock.SelectedValue));
-
                 if (dt.Rows.Count > 0)
                 {
-
                     if (Convert.ToDecimal(txt_money.Text) > Convert.ToDecimal(dt.Rows[0][0]))
                     {
                         MessageBox.Show("رصيد الخزنة الحالى غير كافى ");
@@ -148,8 +139,9 @@ namespace Laboratory.PL
 
                 i.Addinstallment(dateTimePicker1.Value, Convert.ToDecimal(txt_money.Text), txt_username.Text, Convert.ToInt32(cmb_stock.SelectedValue), Convert.ToInt32(cmb_instalmenttype.SelectedValue));
 
-                MessageBox.Show("تم اضافه بيانات الطبيب بنجاح", "عمليه الاضافه", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-                s.Add_StockPull(Convert.ToInt32(cmb_stock.SelectedValue), Convert.ToDecimal(txt_money.Text), dateTimePicker1.Value, txt_username.Text, cmb_instalmenttype.Text + " " + " دفع قسط");
+                MessageBox.Show("تم اضافةالقسط بنجاح ", "عمليه الاضافه", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                s.Add_StockPull(Convert.ToInt32(cmb_stock.SelectedValue), Convert.ToDecimal(txt_money.Text),
+                             dateTimePicker1.Value, txt_username.Text,  " دفع قسط" +" "+ cmb_instalmenttype.Text );
 
                 dataGridView1.DataSource = i.Selectinstallment();
                 clear();
@@ -157,81 +149,75 @@ namespace Laboratory.PL
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
-              
+                MessageBox.Show(ex.StackTrace);              
             }
-
-
         }
 
         private void simpleButton2_Click(object sender, EventArgs e)
         {
             try
             {
-
-
-                if (txt_money.Text == "")
+                if (txt_money.Text == "" ||txt_money.Text=="0" )
                 {
                     MessageBox.Show("برجاء التاكد من المبلغ");
+                    txt_money.Focus();
                     return;
-
                 }
                 dt.Clear();
-
                 dt = s.Select_moneyStock(Convert.ToInt32(cmb_stock.SelectedValue));
-
                 if (dt.Rows.Count > 0)
                 {
-
                     if (Convert.ToDecimal(txt_money.Text) > Convert.ToDecimal(dt.Rows[0][0]))
                     {
                         MessageBox.Show("رصيد الخزنة الحالى غير كافى ");
                         return;
                     }
                 }
-
-
-                if (MessageBox.Show("هل تريد تعديل بيانات الطبيب", "عمليه التعديل", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes)
+                if (MessageBox.Show("هل تريد مسح القسط المحدد", "عمليه مسح قسط", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes)
                 {
-                    s.Add_StockPull(Convert.ToInt32(cmb_stock.SelectedValue), Convert.ToDecimal(dataGridView1.CurrentRow.Cells[3].Value), dateTimePicker1.Value, txt_username.Text, cmb_instalmenttype.Text + " " + " دفع قسط");
-                    i.Updateinstallment(dateTimePicker1.Value, Convert.ToDecimal(txt_money.Text), Convert.ToInt32(cmb_instalmenttype.SelectedValue), Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value));
-                    s.add_insertStock(Convert.ToInt32(cmb_stock.SelectedValue), Convert.ToDecimal(txt_money.Text), dateTimePicker1.Value, txt_username.Text, cmb_instalmenttype.Text + " " + " دفع قسط");
+                    //s.Add_StockPull(Convert.ToInt32(cmb_stock.SelectedValue), Convert.ToDecimal(dataGridView1.CurrentRow.Cells[3].Value), dateTimePicker1.Value, txt_username.Text, cmb_instalmenttype.Text + " " + " دفع قسط");
+                    i.Delete_Installment(Convert.ToInt32(textBox1.Text));
+                    s.add_insertStock(Convert.ToInt32(cmb_stock.SelectedValue), Convert.ToDecimal(txt_money.Text), 
+                        dateTimePicker1.Value, txt_username.Text, " مسح قسط" + " " + cmb_instalmenttype.Text );
 
-                    MessageBox.Show("تم التعديل بنجاح", "عمليه التعديل", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-
-
-                    dataGridView1.DataSource = i.Selectinstallment();
-                   
-                    clear();
-
+                    MessageBox.Show("تم مسح القسط بنجاح", "عمليه التعديل", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                 }
                 else
                 {
-                    MessageBox.Show("تم الغاء عمليه التعديل");
-                 
-                    clear();
-
-              
-              
+                    MessageBox.Show("تم الغاء عمليه المسح");
                 }
-
+                dataGridView1.DataSource = i.Selectinstallment();
+                clear();
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
+                MessageBox.Show(ex.StackTrace);
             }
-
-
-
         }
 
         private void txt_username_TextChanged(object sender, EventArgs e)
         {
 
         }
-
         private void txt_money_TextChanged(object sender, EventArgs e)
         {
+        }
 
+        private void txt_money_MouseLeave(object sender, EventArgs e)
+        {
+            if (txt_money.Text=="")
+            {
+                txt_money.Text = "0";
+            }
+        }
+
+        private void txt_money_Click(object sender, EventArgs e)
+        {
+            if (txt_money.Text=="0")
+            {
+                txt_money.Text ="";
+            }
         }
     }
 }
