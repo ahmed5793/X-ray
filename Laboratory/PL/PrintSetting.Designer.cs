5489276b68d8a824c32c821f5468bd65cbb10c89
ahmed mahmmed abd el twab <Ahmed5793>
2020-04-07 20:23:13 +0200

    kljfekf

---
+++ Laboratory/PL/PrintSetting.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Laboratory.PL
+{
+    partial class PrintSetting
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PrintSetting));
+            this.bunifuCustomLabel1 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuCustomLabel2 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.cmb_barcode = new System.Windows.Forms.ComboBox();
+            this.cmb_invoice = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // bunifuCustomLabel1
+            // 
+            this.bunifuCustomLabel1.AutoSize = true;
+            this.bunifuCustomLabel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.bunifuCustomLabel1.Font = new System.Drawing.Font("Droid Arabic Kufi", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel1.ForeColor = System.Drawing.Color.White;
+            this.bunifuCustomLabel1.Location = new System.Drawing.Point(365, 60);
+            this.bunifuCustomLabel1.Name = "bunifuCustomLabel1";
+            this.bunifuCustomLabel1.Size = new System.Drawing.Size(147, 36);
+            this.bunifuCustomLabel1.TabIndex = 2;
+            this.bunifuCustomLabel1.Text = "طابعه الباركود";
+            // 
+            // bunifuCustomLabel2
+            // 
+            this.bunifuCustomLabel2.AutoSize = true;
+            this.bunifuCustomLabel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.bunifuCustomLabel2.Font = new System.Drawing.Font("Droid Arabic Kufi", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel2.ForeColor = System.Drawing.Color.White;
+            this.bunifuCustomLabel2.Location = new System.Drawing.Point(365, 132);
+            this.bunifuCustomLabel2.Name = "bunifuCustomLabel2";
+            this.bunifuCustomLabel2.Size = new System.Drawing.Size(150, 36);
+            this.bunifuCustomLabel2.TabIndex = 3;
+            this.bunifuCustomLabel2.Text = "طابعه الفاتورة";
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Appearance.BackColor = System.Drawing.Color.Crimson;
+            this.simpleButton1.Appearance.Font = new System.Drawing.Font("Droid Arabic Kufi", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.simpleButton1.Appearance.Options.UseBackColor = true;
+            this.simpleButton1.Appearance.Options.UseFont = true;
+            this.simpleButton1.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.ImageOptions.Image")));
+            this.simpleButton1.Location = new System.Drawing.Point(178, 246);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(186, 43);
+            this.simpleButton1.TabIndex = 6;
+            this.simpleButton1.Text = "حفظ";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // cmb_barcode
+            // 
+            this.cmb_barcode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmb_barcode.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.cmb_barcode.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cmb_barcode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmb_barcode.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_barcode.FormattingEnabled = true;
+            this.cmb_barcode.Location = new System.Drawing.Point(12, 60);
+            this.cmb_barcode.Name = "cmb_barcode";
+            this.cmb_barcode.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.cmb_barcode.Size = new System.Drawing.Size(333, 32);
+            this.cmb_barcode.TabIndex = 217;
+            // 
+            // cmb_invoice
+            // 
+            this.cmb_invoice.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmb_invoice.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.cmb_invoice.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cmb_invoice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmb_invoice.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_invoice.FormattingEnabled = true;
+            this.cmb_invoice.Location = new System.Drawing.Point(12, 134);
+            this.cmb_invoice.Name = "cmb_invoice";
+            this.cmb_invoice.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.cmb_invoice.Size = new System.Drawing.Size(333, 30);
+            this.cmb_invoice.TabIndex = 218;
+            // 
+            // PrintSetting
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.ClientSize = new System.Drawing.Size(527, 303);
+            this.Controls.Add(this.cmb_invoice);
+            this.Controls.Add(this.cmb_barcode);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.bunifuCustomLabel2);
+            this.Controls.Add(this.bunifuCustomLabel1);
+            this.Name = "PrintSetting";
+            this.Text = "PrintSetting";
+            this.Load += new System.EventHandler(this.PrintSetting_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel1;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        public System.Windows.Forms.ComboBox cmb_barcode;
+        public System.Windows.Forms.ComboBox cmb_invoice;
+    }
+}
---
+++ Laboratory/PL/PrintSetting.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Printing;
+using Laboratory.BL;
+
+namespace Laboratory.PL
+{
+    public partial class PrintSetting : Form
+    {
+        DataTable dt = new DataTable();
+        Users u = new Users();
+        public PrintSetting()
+        {
+            InitializeComponent();
+          
+            
+
+            print();
+            GetSetting();
+
+        }
+
+        void GetSetting()
+        {
+            cmb_barcode.Text = Properties.Settings.Default.PrintNameBarcode;
+
+
+            cmb_invoice.Text = Properties.Settings.Default.PrintNameInvoice;
+        }
+        void saveSetting()
+        {
+            Properties.Settings.Default.PrintNameBarcode = cmb_barcode.Text;
+
+
+            Properties.Settings.Default.PrintNameInvoice = cmb_invoice.Text;
+            Properties.Settings.Default.Save();
+        }
+        
+        void print()
+        {
+            try
+            {
+                foreach (string item in PrinterSettings.InstalledPrinters)
+                {
+
+                    cmb_barcode.Items.Add(item);
+                    cmb_invoice.Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+           
+        }
+           
+        private void PrintSetting_Load(object sender, EventArgs e)
+        {
+
+        }
+   
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            if (cmb_barcode.Text=="")
+            {
+                MessageBox.Show("يرجي اختيار طابعه لطابعه الباركود");
+                return;
+            }
+            if (cmb_invoice.Text == "")
+            
+            {
+                    MessageBox.Show("يرجي اختيار طابعه لطابعه الفاتورة");
+                    return;
+            }
+            else
+            {
+                //    cmb_barcode.Text = Properties.Settings.Default.PrintNameBarcode;
+                //    cmb_barcode.Text = Properties.Settings.Default.PrintNameInvoice;
+
+                saveSetting();
+                MessageBox.Show("تم الحفظ بنجاح");
+                cmb_barcode.SelectedIndex = -1;
+                cmb_invoice.SelectedIndex = -1;
+
+
+
+            }
+           
+
+        }
+    }
+}
