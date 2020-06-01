44749a25b30b2e64ea4ba83bdb056930a2e9ec40
ahmed mahmmed abd el twab <Ahmed5793>
2020-06-01 19:01:27 +0200

    jhjk

--- Laboratory/DAL/DataAccessLayer.cs
+++ Laboratory/DAL/DataAccessLayer.cs
@@ -17,13 +17,13 @@ namespace Laboratory.DAL
         {
 
 
-
-            //   con = new SqlConnection(@"server =.; database=Laboratory_las;integrated security=true");
-            //con = new SqlConnection("Data Source=sql5031.site4now.net; database=DB_A54A03_Laboratory;User Id=DB_A54A03_Laboratory_admin;Password=Titonasser1994;") ;
-
             //demo
+          //  con = new SqlConnection(@"server =.; database=Laboratory_las;integrated security=true");
+           con = new SqlConnection("Data Source=sql5046.site4now.net; database=DB_A54A03_TestLaboratory;User Id=DB_A54A03_TestLaboratory_admin;Password=Titonasser1994;") ;
 
-               con = new SqlConnection("Data Source=sql5033.site4now.net; database=DB_A54A03_LastLaboratory;User Id=  DB_A54A03_LastLaboratory_admin;Password=Titonasser1994;") ;
+            //last
+
+            //   con = new SqlConnection("Data Source=sql5033.site4now.net; database=DB_A54A03_LastLaboratory;User Id=  DB_A54A03_LastLaboratory_admin;Password=Titonasser1994;") ;
 
           
         }
