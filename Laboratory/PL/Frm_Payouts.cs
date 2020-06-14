4840c1ac7d35b8946e21413661e5e1ea1a5e07c8
ahmed mahmmed abd el twab <Ahmed5793>
2020-06-14 23:33:42 +0200

    jhbjnm

--- Laboratory/PL/Frm_Payouts.cs
+++ Laboratory/PL/Frm_Payouts.cs
@@ -94,8 +94,10 @@ namespace Laboratory.PL
                 {
                     //if (Convert.ToDecimal(txt_pay.Text) == Convert.ToDecimal(dt5.Rows[0][1]))
                     //{
-                        MessageBox.Show("عزيزى المستخدم يرجي العلم باان تم استرداد مبلغ الفاتورة من قبل لايمكن استرداها مرة اخرى   ", "", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
-                        return;
+                      //  MessageBox.Show("عزيزى المستخدم يرجي العلم باان تم استرداد مبلغ الفاتورة من قبل لايمكن استرداها مرة اخرى   ", "", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                    MessageBox.Show(dt5.Rows[0][3].ToString() , "", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+
+                    return;
                    //}
                 }
                 dt10.Clear();
